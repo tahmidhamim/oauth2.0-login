d59d0fa3b30063b7346982c827b627b7dc829401
Tahmid Hamim <tahmidhamim@Tahmids-MacBook-Air.local>
2024-05-26 18:41:40 +0600

    Update session config

--- server/api/index.js
+++ server/api/index.js
@@ -19,6 +19,11 @@ app.use(cors({
     credentials: true
 }));
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+    app.set('trust proxy', 1); // Trust first proxy
+}
+
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
